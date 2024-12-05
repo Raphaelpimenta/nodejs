e1c3e21093273ff0d9e58caa165fee393bd0adf8
Raphaelpimenta <raphael.pimentagomes@hotmail.com>
2024-12-04 23:44:33 -0300

    conceito de streams em node

---
+++ streams/fundamentos.js
@@ -0,0 +1,27 @@
+import { Readable } from 'node:stream'
+
+
+class OneToHundredStream extends Readable {
+
+    index = 1
+
+    _read() {
+
+        setTimeout(() => {
+
+            const i = this.index++
+
+            if(i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+    
+                this.push(buf)
+            }
+        }, 1000)
+    }
+
+}
+
+new OneToHundredStream()
+.pipe(process.stdout)
