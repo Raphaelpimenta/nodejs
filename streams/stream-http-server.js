7dc914f196fb31a9da9cb0697537a5440b31ba48
Raphael Pimenta Gomes <raphael.pimentagomes@hotmail.com>
2024-12-05 16:10:13 -0300

    consumo de uma stream completa

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -17,10 +17,27 @@ class InverseNumberStream extends Transform {
 // req => ReadableStream
 // res => WritableStream
 
-const server = http.createServer((req, res) => {
-    return req
-        .pipe(new InverseNumberStream())
-        .pipe(res)
+const server = http.createServer(async (req, res) => {
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    // {"nome": "raphael", "email": "raphael@exemple.com"}
+    // {"nome": "raphael", "em
+
+
+    const fullStreamContent = Buffer.concat(buffers).toString()
+
+    console.log(fullStreamContent)
+
+    return res.end(fullStreamContent)
+
+
+    // return req
+    //     .pipe(new InverseNumberStream())
+    //     .pipe(res)
 })
 
 server.listen(3334)
